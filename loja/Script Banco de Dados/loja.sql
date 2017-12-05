59b67c6f7e205d8dc725d4ccda802d235bdfc14f
Vinicius Loreto Ferreira <vini.loreto@gmail.com>
2017-12-05 13:42:11 -0200

    mensagem

--- loja/Script Banco de Dados/loja.sql
+++ loja/Script Banco de Dados/loja.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 04-Dez-2017 às 19:37
+-- Generation Time: 05-Dez-2017 às 16:39
 -- Versão do servidor: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -30,11 +30,10 @@ USE `loja`;
 -- Estrutura da tabela `categorias`
 --
 
-CREATE TABLE IF NOT EXISTS `categorias` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `categorias`
@@ -52,30 +51,27 @@ INSERT INTO `categorias` (`id`, `nome`) VALUES
 -- Estrutura da tabela `produtos`
 --
 
-CREATE TABLE IF NOT EXISTS `produtos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `produtos` (
+  `id` int(11) NOT NULL,
   `nome` varchar(255) DEFAULT NULL,
   `preco` decimal(10,2) DEFAULT NULL,
   `descricao` text,
   `categoria_id` int(11) DEFAULT NULL,
   `usado` tinyint(1) DEFAULT '0',
-  `fk_user` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `produto_fk1` (`fk_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=latin1;
+  `fk_user` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `produtos`
 --
 
 INSERT INTO `produtos` (`id`, `nome`, `preco`, `descricao`, `categoria_id`, `usado`, `fk_user`) VALUES
-(2, 'Barco', '5000.00', 'Descricao deste produto', 1, 0, 1),
+(2, 'Barco', '5000.00', 'Descricao deste produto', 3, 0, 1),
 (3, 'Caneta', '2.00', 'Descricao deste produto', 2, 0, 1),
-(18, 'Motocicleta', '10000.00', 'Descricao deste produto', 3, 0, 1),
+(18, 'Motocicleta', '10000.00', 'Descricao deste produto', 2, 0, 1),
 (21, 'Bola', '90.00', 'Descricao deste produto', 1, 0, 1),
 (22, 'Bolo', '40.00', 'Descricao deste produto', 4, 0, 1),
 (26, 'Chocolate', '15.00', 'Chocolate', 4, 0, 1),
-(27, 'Motocicleta', '5000.00', 'Motin', 1, 0, NULL),
 (29, 'Tenis', '200.00', 'Tenis de corrida', 1, 0, 1),
 (30, 'Bike', '2001.00', 'Bike monstra', 1, 0, 9);
 
@@ -85,22 +81,67 @@ INSERT INTO `produtos` (`id`, `nome`, `preco`, `descricao`, `categoria_id`, `usa
 -- Estrutura da tabela `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `nm_user` varchar(255) DEFAULT NULL,
-  `cd_user` int(11) NOT NULL AUTO_INCREMENT,
+  `cd_user` int(11) NOT NULL,
   `nm_login_user` varchar(255) DEFAULT NULL,
   `cd_password_user` varchar(255) DEFAULT NULL,
-  `foto` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`cd_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+  `foto` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `user`
 --
 
 INSERT INTO `user` (`nm_user`, `cd_user`, `nm_login_user`, `cd_password_user`, `foto`) VALUES
-('Administrador', 1, 'admin', '1234', '15123564955a24ba8f4a924.jpg'),
-('vinicius', 9, 'vini', '1234', 'NULL');
+('Administrador', 1, 'admin', '1234', '15124806215a269f6dd508a.jpg'),
+('vinicius', 9, 'vini', '1234', '15124805835a269f47e4765.jpg'),
+('Vinicius', 10, 'viniloreto', '1234', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `produtos`
+--
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `produto_fk1` (`fk_user`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`cd_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `produtos`
+--
+ALTER TABLE `produtos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `cd_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Constraints for dumped tables
